916995af97d07836067dcdc3fcd441af95112378
Byungjun Park <hexarf@gmail.com>
2024-06-25 23:56:34 +0900

    add zip operators extend

--- Sources/RxSwiftExtensions/RxSwiftExtensions.swift
+++ Sources/RxSwiftExtensions/RxSwiftExtensions.swift
@@ -194,4 +194,158 @@ class ObservableExtension {
             return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second)
         }
     }
+
+    /// 10
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(_ source1: Observable<T1>,
+                                                                       _ source2: Observable<T2>,
+                                                                       _ source3: Observable<T3>,
+                                                                       _ source4: Observable<T4>,
+                                                                       _ source5: Observable<T5>,
+                                                                       _ source6: Observable<T6>,
+                                                                       _ source7: Observable<T7>,
+                                                                       _ source8: Observable<T8>,
+                                                                       _ source9: Observable<T9>,
+                                                                       _ source10: Observable<T10>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1)
+        }
+    }
+
+    /// 11
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(_ source1: Observable<T1>,
+                                                                            _ source2: Observable<T2>,
+                                                                            _ source3: Observable<T3>,
+                                                                            _ source4: Observable<T4>,
+                                                                            _ source5: Observable<T5>,
+                                                                            _ source6: Observable<T6>,
+                                                                            _ source7: Observable<T7>,
+                                                                            _ source8: Observable<T8>,
+                                                                            _ source9: Observable<T9>,
+                                                                            _ source10: Observable<T10>,
+                                                                            _ source11: Observable<T11>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10, source11)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10, T11)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1, second.2)
+        }
+    }
+
+    /// 12
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(_ source1: Observable<T1>,
+                                                                                 _ source2: Observable<T2>,
+                                                                                 _ source3: Observable<T3>,
+                                                                                 _ source4: Observable<T4>,
+                                                                                 _ source5: Observable<T5>,
+                                                                                 _ source6: Observable<T6>,
+                                                                                 _ source7: Observable<T7>,
+                                                                                 _ source8: Observable<T8>,
+                                                                                 _ source9: Observable<T9>,
+                                                                                 _ source10: Observable<T10>,
+                                                                                 _ source11: Observable<T11>,
+                                                                                 _ source12: Observable<T12>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10, source11, source12)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10, T11, T12)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1, second.2, second.3)
+        }
+    }
+
+    /// 13
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(_ source1: Observable<T1>,
+                                                                                      _ source2: Observable<T2>,
+                                                                                      _ source3: Observable<T3>,
+                                                                                      _ source4: Observable<T4>,
+                                                                                      _ source5: Observable<T5>,
+                                                                                      _ source6: Observable<T6>,
+                                                                                      _ source7: Observable<T7>,
+                                                                                      _ source8: Observable<T8>,
+                                                                                      _ source9: Observable<T9>,
+                                                                                      _ source10: Observable<T10>,
+                                                                                      _ source11: Observable<T11>,
+                                                                                      _ source12: Observable<T12>,
+                                                                                      _ source13: Observable<T13>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10, source11, source12, source13)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10, T11, T12, T13)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1, second.2, second.3, second.4)
+        }
+    }
+
+    /// 14
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(_ source1: Observable<T1>,
+                                                                                           _ source2: Observable<T2>,
+                                                                                           _ source3: Observable<T3>,
+                                                                                           _ source4: Observable<T4>,
+                                                                                           _ source5: Observable<T5>,
+                                                                                           _ source6: Observable<T6>,
+                                                                                           _ source7: Observable<T7>,
+                                                                                           _ source8: Observable<T8>,
+                                                                                           _ source9: Observable<T9>,
+                                                                                           _ source10: Observable<T10>,
+                                                                                           _ source11: Observable<T11>,
+                                                                                           _ source12: Observable<T12>,
+                                                                                           _ source13: Observable<T13>,
+                                                                                           _ source14: Observable<T14>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10, source11, source12, source13, source14)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10, T11, T12, T13, T14)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1, second.2, second.3, second.4, second.5)
+        }
+    }
+
+    /// 15
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(_ source1: Observable<T1>,
+                                                                                                _ source2: Observable<T2>,
+                                                                                                _ source3: Observable<T3>,
+                                                                                                _ source4: Observable<T4>,
+                                                                                                _ source5: Observable<T5>,
+                                                                                                _ source6: Observable<T6>,
+                                                                                                _ source7: Observable<T7>,
+                                                                                                _ source8: Observable<T8>,
+                                                                                                _ source9: Observable<T9>,
+                                                                                                _ source10: Observable<T10>,
+                                                                                                _ source11: Observable<T11>,
+                                                                                                _ source12: Observable<T12>,
+                                                                                                _ source13: Observable<T13>,
+                                                                                                _ source14: Observable<T14>,
+                                                                                                _ source15: Observable<T15>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10, source11, source12, source13, source14, source15)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10, T11, T12, T13, T14, T15)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1, second.2, second.3, second.4, second.5, second.6)
+        }
+    }
+
+    /// 16
+    static func zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(_ source1: Observable<T1>,
+                                                                                                     _ source2: Observable<T2>,
+                                                                                                     _ source3: Observable<T3>,
+                                                                                                     _ source4: Observable<T4>,
+                                                                                                     _ source5: Observable<T5>,
+                                                                                                     _ source6: Observable<T6>,
+                                                                                                     _ source7: Observable<T7>,
+                                                                                                     _ source8: Observable<T8>,
+                                                                                                     _ source9: Observable<T9>,
+                                                                                                     _ source10: Observable<T10>,
+                                                                                                     _ source11: Observable<T11>,
+                                                                                                     _ source12: Observable<T12>,
+                                                                                                     _ source13: Observable<T13>,
+                                                                                                     _ source14: Observable<T14>,
+                                                                                                     _ source15: Observable<T15>,
+                                                                                                     _ source16: Observable<T16>)
+    -> Observable<(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16)> {
+        let firstCombined = Observable.zip(source1, source2, source3, source4, source5, source6, source7, source8)
+        let secondCombined = Observable.zip(source9, source10, source11, source12, source13, source14, source15, source16)
+        return Observable.zip(firstCombined, secondCombined) { (first: (T1, T2, T3, T4, T5, T6, T7, T8), second: (T9, T10, T11, T12, T13, T14, T15, T16)) in
+            return (first.0, first.1, first.2, first.3, first.4, first.5, first.6, first.7, second.0, second.1, second.2, second.3, second.4, second.5, second.6, second.7)
+        }
+    }
 }
--- Tests/RxSwiftExtensionsTests/RxSwiftExtensionsTests.swift
+++ Tests/RxSwiftExtensionsTests/RxSwiftExtensionsTests.swift
@@ -337,4 +337,277 @@ final class RxSwiftExtensionsTests: XCTestCase {
 
         wait(for: [expectation], timeout: 1.0)
     }
+
+    func testZip10() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+
+        let combinedObservable = ObservableExtension.zip(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testZip11() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+        let observable11 = Observable.just(11)
+
+        let combinedObservable = ObservableExtension.combineLatest(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10, observable11)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            XCTAssertEqual(values.10, 11)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testZip12() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+        let observable11 = Observable.just(11)
+        let observable12 = Observable.just(12)
+
+        let combinedObservable = ObservableExtension.combineLatest(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10, observable11, observable12)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            XCTAssertEqual(values.10, 11)
+            XCTAssertEqual(values.11, 12)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testZip13() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+        let observable11 = Observable.just(11)
+        let observable12 = Observable.just(12)
+        let observable13 = Observable.just(13)
+
+        let combinedObservable = ObservableExtension.combineLatest(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10, observable11, observable12, observable13)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            XCTAssertEqual(values.10, 11)
+            XCTAssertEqual(values.11, 12)
+            XCTAssertEqual(values.12, 13)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testZip14() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+        let observable11 = Observable.just(11)
+        let observable12 = Observable.just(12)
+        let observable13 = Observable.just(13)
+        let observable14 = Observable.just(14)
+
+        let combinedObservable = ObservableExtension.combineLatest(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10, observable11, observable12, observable13, observable14)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            XCTAssertEqual(values.10, 11)
+            XCTAssertEqual(values.11, 12)
+            XCTAssertEqual(values.12, 13)
+            XCTAssertEqual(values.13, 14)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testZip15() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+        let observable11 = Observable.just(11)
+        let observable12 = Observable.just(12)
+        let observable13 = Observable.just(13)
+        let observable14 = Observable.just(14)
+        let observable15 = Observable.just(15)
+
+        let combinedObservable = ObservableExtension.combineLatest(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10, observable11, observable12, observable13, observable14, observable15)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            XCTAssertEqual(values.10, 11)
+            XCTAssertEqual(values.11, 12)
+            XCTAssertEqual(values.12, 13)
+            XCTAssertEqual(values.13, 14)
+            XCTAssertEqual(values.14, 15)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testZip16() {
+        let observable1 = Observable.just(1)
+        let observable2 = Observable.just(2)
+        let observable3 = Observable.just(3)
+        let observable4 = Observable.just(4)
+        let observable5 = Observable.just(5)
+        let observable6 = Observable.just(6)
+        let observable7 = Observable.just(7)
+        let observable8 = Observable.just(8)
+        let observable9 = Observable.just(9)
+        let observable10 = Observable.just(10)
+        let observable11 = Observable.just(11)
+        let observable12 = Observable.just(12)
+        let observable13 = Observable.just(13)
+        let observable14 = Observable.just(14)
+        let observable15 = Observable.just(15)
+        let observable16 = Observable.just(16)
+
+        let combinedObservable = ObservableExtension.combineLatest(observable1, observable2, observable3, observable4, observable5, observable6, observable7, observable8, observable9, observable10, observable11, observable12, observable13, observable14, observable15, observable16)
+
+        let expectation = XCTestExpectation(description: "CombineLatest should emit the correct tuple")
+
+        _ = combinedObservable.subscribe(onNext: { values in
+            XCTAssertEqual(values.0, 1)
+            XCTAssertEqual(values.1, 2)
+            XCTAssertEqual(values.2, 3)
+            XCTAssertEqual(values.3, 4)
+            XCTAssertEqual(values.4, 5)
+            XCTAssertEqual(values.5, 6)
+            XCTAssertEqual(values.6, 7)
+            XCTAssertEqual(values.7, 8)
+            XCTAssertEqual(values.8, 9)
+            XCTAssertEqual(values.9, 10)
+            XCTAssertEqual(values.10, 11)
+            XCTAssertEqual(values.11, 12)
+            XCTAssertEqual(values.12, 13)
+            XCTAssertEqual(values.13, 14)
+            XCTAssertEqual(values.14, 15)
+            XCTAssertEqual(values.15, 16)
+            expectation.fulfill()
+        })
+
+        wait(for: [expectation], timeout: 1.0)
+    }
 }
